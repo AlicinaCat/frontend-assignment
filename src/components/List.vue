6b0479f209ef662e05778980b631ab931bb35ba5
alicinacat <alice.casaburi@gmail.com>
2024-11-06 14:19:55 +0100

    Added sorting of albums descending by year

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,11 +1,16 @@
 <template>
   <ul role="list" class="divide-y divide-gray-100 bg-white rounded-md p-4 m-4">
-    <ListItem v-for="album in albums" :key="album.name" :album="album" />
+    <ListItem v-for="album in sortedAlbums" :key="album.name" :album="album" />
   </ul>
 </template>
 
 <script setup>
+  import { computed } from 'vue';
   import data from '../data/albums.json';
   import ListItem from './ListItem.vue';
   const albums = data.albums;
+
+  const sortedAlbums = computed(() => {
+    return albums.sort((a, b) => new Date(b.year) - new Date(a.year));
+  })
 </script>
